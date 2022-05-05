ca2ea07057551dacf10d5e285c92fcf5da8b98b3
Abiel Magu <abielmagu@mail.com>
2022-05-05 15:47:46 -0500

    Actualiza view index de Job

--- resources/views/jobs/index.blade.php
+++ resources/views/jobs/index.blade.php
@@ -1,15 +1,14 @@
 @extends('layouts.app')
 @section('content')
-    <h1>Jobs ({{ $jobs->count() }})</h1>
-    <a href="{{ route('jobs.create') }}">Create</a>
-    <br>
-    <ul>
-        @foreach($jobs as $job)
-        <li>
-            <strong>{{ $job->name }}</strong>
-            <a href="{{ route('jobs.show', $job) }}">Show</a>
-            <small>{{ $job->isAvailable() ? 'Yes' : 'No' }}</small>
-        </li>
-        @endforeach
-    </ul>
+<h1>Jobs ({{ $jobs->count() }})</h1>
+<a href="{{ route('jobs.create') }}">Create</a>
+<ul>
+    @foreach($jobs as $job)
+    <li>
+        <span>{{ $job->name }}</span>
+        <a href="{{ route('jobs.show', $job) }}">Show</a>
+        <small>{{ $job->isAvailable() ? 'Yes' : 'No' }}</small>
+    </li>
+    @endforeach
+</ul>
 @endsection
