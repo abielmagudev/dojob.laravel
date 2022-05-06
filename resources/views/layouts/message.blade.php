2ed9bb2f3e246042bbaadbb930b388b8698be3bb
Abiel Magu <abielmagu@mail.com>
2022-05-06 03:02:45 -0500

    Actualiza layout message

--- resources/views/layouts/message.blade.php
+++ resources/views/layouts/message.blade.php
@@ -1,7 +1,7 @@
 @foreach(['info','success','warning','danger'] as $message_type)
     @if( session()->has($message_type) )
     <div>
-        <em>{{ session($message_type) }}</em>
+        <em>"{{ session($message_type) }}"</em>
     </div>
     <br>
     @endif
