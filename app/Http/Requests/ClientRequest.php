5a7523eaf08cf84038f2a156d309014ec2918487
Abiel Magu <abielmagu@mail.com>
2022-05-06 03:30:14 -0500

    Actualiza request de Client

--- app/Http/Requests/ClientRequest.php
+++ app/Http/Requests/ClientRequest.php
@@ -32,16 +32,16 @@ class ClientRequest extends FormRequest
     {
         return [
             'name.required' => __('Enter the client\'s name'),
-            'name.string' => __('Enter a valid name for the client'),
+            'name.string' => __('Write a valid name for the client'),
             'lastname.required' => __('Enter the client\'s lastname'),
-            'lastname.string' => __('Enter a valid lastname for the client'),
+            'lastname.string' => __('Write a valid lastname for the client'),
             'address.required' => __('Enter the client\'s address'),
             'zip_code.required' => __('Enter the client\'s zip code'),
             'city.required' => __('Enter the client\'s city'),
             'state.required' => __('Enter the client\'s state'),
             'country.required' => __('Enter the client\'s country'),
             'phone.required' => __('Enter the client\'s phone'),
-            'email.email' => __('Write a valid email of the client'),
+            'email.email' => __('Write a valid email for the client'),
         ];
     }
 
