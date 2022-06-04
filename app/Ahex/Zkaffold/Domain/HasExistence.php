2138bbadfe00db9d07289c8cf8a223a407d5add7
Abiel Magu <abielmagu@mail.com>
2022-06-03 20:35:22 -0500

    Optimiza y mejora trait hasExistence para models

--- app/Ahex/Zkaffold/Domain/HasExistence.php
+++ app/Ahex/Zkaffold/Domain/HasExistence.php
@@ -4,13 +4,13 @@ namespace App\Ahex\Zkaffold\Domain;
 
 trait HasExistence
 {
-    public function isReal()
+    public function isReal(string $property = 'id')
     {
-        return isset($this->id);
+        return isset($this->$property);
     }
 
-    public function isUnreal()
+    public function isUnreal(string $property = 'id')
     {
-        return ! $this->isReal();
+        return ! $this->isReal($property);
     }
 }
