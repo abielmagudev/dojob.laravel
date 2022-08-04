4be10b035a15ad50101e9c8a686acfe68645e0f3
Abiel Magu <abielmagu@mail.com>
2022-08-03 23:47:40 -0500

    Elimina concepto Operator y actualiza navegacion

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,17 +1,16 @@
 <?php
 
 $routes = [
-    'Dashboard' => url('/'),
     'Clients' => route('clients.index'),
     'Crews' => route('crews.index'),
+    'Dashboard' => url('/'),
     'Intermediaries' => route('intermediaries.index'),
     'Jobs' => route('jobs.index'),
-    'Operators' => route('operators.index'),
     'Members' => route('members.index'),
     'Skills' => route('skills.index'),
+    'Users' => route('users.index'),
     'Warranties' => route('warranties.index'),
     'Works' => route('works.index'),
-    'Users' => route('users.index'),
 ];
 
 ?>
