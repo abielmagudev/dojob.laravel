13ac647f5e74f7fc107f52104f20f0cc75e748af
Abiel Magu <abielmagu@mail.com>
2022-10-05 02:04:20 -0500

    Actualiza tema y agrega funcionalidad js a Bootstrap CSS

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,1 @@
-window._ = require("lodash");
 require('./bootstrap');
-/*import "bootstrap";*/
-import 'bootswatch/dist/superhero/bootstrap.min.css';
--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -26,3 +26,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 //     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
 //     forceTLS: true
 // });
+
+// Css Bootstrap Framework
+require('bootstrap/dist/js/bootstrap.bundle')
+import 'bootswatch/dist/litera/bootstrap.min.css';
+// import "bootstrap";
