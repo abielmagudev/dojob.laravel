95cb1b042a6b392389b58d0014aa821bd2e8e274
Abiel Magu <abielmagu@mail.com>
2022-10-26 00:05:45 -0500

    Actualiza el layout app

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,31 +7,52 @@
     <title>{{ config('app.name') }}</title>
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
-<body>
-    @auth   
-    <div style="text-align:right">
-        <span>{{ auth()->user()->profile->fullname }}</span>
-        <form action="{{ route('logout') }}" method="post" style="display:inline-block">
-            @csrf
-            <button class='btn btn-danger' type="submit">Logout</button>
-        </form>
-    </div>
+<body class="bg-light"> 
+    @auth 
+    <nav class="navbar navbar-dark bg-primary">
+        <div class="container-fluid">
+            <span class="navbar-brand mb-0 h1">Navbar</span>
+            <div class="dropdown">
+                <button class="btn border-0 btn-small text-white dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    <span>{{ auth()->user()->profile->fullname }}</span>
+                </button>
+                <ul class="dropdown-menu">
+                    <li>
+                        <a class="dropdown-item" href="#">Account</a>
+                    </li>
+                    <li>
+                        <a class="dropdown-item" href="#">Settings</a>
+                    </li>
+                    <li>
+                        <button class='dropdown-item' type="submit" form='formLogout'>Logout</button>
+                        <form action="{{ route('logout') }}" method="post" id='formLogout'>
+                            @csrf
+                        </form>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
     @endauth
 
-    <div class="container">
-        @if(! isset($navigation_hidden) )      
-        <div style="display:inline-block; vertical-align:top; margin-right:2rem">
-            @include('layouts.navigation')
-        </div>
-        @endif
-    
-        <div style="display:inline-block; word-break:break-all">
-            @include('layouts.message')
-            @include('layouts.errors')
-            @yield('content')
+    <div class="container-fluid">
+        <div class='row vh-100'>
+            <div class="col-sm col-sm-2 p-0">      
+                @if(! isset($navigation_hidden) )      
+                    @include('layouts.navigation')
+                @endif
+            </div>
+            <div class="col-sm p-3">
+                @include('layouts.message')
+                @include('layouts.errors')
+                @yield('content')
+                <br>
+                <br>
+            </div>
         </div>
     </div>
 
+    <script src="{{ asset('js/app.js') }}"></script>
     @stack('scripts')
 </body>
 </html>
