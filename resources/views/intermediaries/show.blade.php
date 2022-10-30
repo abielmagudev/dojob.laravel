985a0d562452965821381e33ab4cd202681973d9
Abiel Magu <abielmagu@mail.com>
2022-10-29 20:26:57 -0500

    Actualiza show de Intermediary

--- resources/views/intermediaries/show.blade.php
+++ resources/views/intermediaries/show.blade.php
@@ -5,7 +5,7 @@
     <x-slot name='subtitle'>{{ $intermediary->contact }}</x-slot>
 </x-heading>
 <p class="text-muted">
-    <span class='d-block mb-3'>{{ $intermediary->phone }}; {{ $intermediary->email }}</span>
+    <span class='d-block mb-3'>{{ $intermediary->phone }} | {{ $intermediary->email }}</span>
     @if( $intermediary->hasNotes() )
     <em class='d-block'>{{ $intermediary->notes }}</em>
     @endif
@@ -13,4 +13,7 @@
 <p class="text-end">
     <a href="{{ route('intermediaries.edit', $intermediary) }}" class='btn btn-warning'>Edit intermediary</a>
 </p>
+@if(! $intermediary->isAvailable() )   
+<p class="h1 text-muted text-center">Unavailable</p>
+@endif
 @endsection
