145bb254576091aac2253b30dc56b0496e33dbde
Abiel Magu <abielmagu@mail.com>
2022-10-30 00:22:32 -0500

    Actualiza views de Plugin

--- resources/views/plugins/edit.blade.php
+++ resources/views/plugins/edit.blade.php
@@ -1,35 +1,56 @@
 @extends('app')
 @section('content')
-<p>Plugin</p>
-<h1>{{ $plugin->name }}</h1>
-<form action="{{ route('plugins.update', $plugin) }}" method="post">
-    @csrf
-    @method('patch')
-    <div>
-        <label for="">Settings</label>
-        @foreach(['one' => 'uno', 'two' => 'dos'] as $name => $value)
-        <?php $checkbox_id = 'checkbox' . ucfirst($name) . 'Setting' ?>
-        <div>
-            <input type="checkbox" name="settings[{{ $name }}]" value="{{ $value }}" id="{{ $checkbox_id }}" {{ $plugin->hasSetting($name) ? 'checked' : '' }}>
-            <label for="{{ $checkbox_id }}">{{ ucfirst($name) }}</label> 
-        </div>
-        @endforeach
+<x-heading>
+    {{ $plugin->name }}
+    <x-slot name='subtitle'>Plugin</x-slot>
+</x-heading>
+<div class="card">
+    <div class="card-header">
+        <span class="text-uppercase">Edit plugin</span>
     </div>
-    <br>
-    <div>
-        <input type="checkbox" name="enabled" value="yes" id="checkboxEnabled" {{ $plugin->isEnabled() ? 'checked' : '' }}>
-        <label for="checkboxEnabled">Enabled</label>
-        <br>
-        <small>You will not be able to use the plugin, but it will keep and display the saved information.</small>
+    <div class="card-body">
+        <form action="{{ route('plugins.update', $plugin) }}" method="post">
+            @csrf
+            @method('patch')
+            <div class='mb-3'>
+                <div class='form-label'>Settings</div>
+                <div class="border rounded p-3">
+                    @foreach(['one' => 'uno', 'two' => 'dos'] as $name => $value)
+                    <?php $checkbox_id = 'checkbox' . ucfirst($name) . 'Setting' ?>
+                    <div>
+                        <input class='form-check-input' type="checkbox" name="settings[{{ $name }}]" value="{{ $value }}" id="{{ $checkbox_id }}" {{ $plugin->hasSetting($name) ? 'checked' : '' }}>
+                        <label class='form-check-label' for="{{ $checkbox_id }}">{{ ucfirst($name) }}</label> 
+                    </div>
+                    @endforeach
+                </div>
+            </div>
+            <div class='mb-3'>
+                <label for="checkboxEnabled" class='form-label'>Enabled</label>
+                <div class="border rounded p-3">
+                    <input class='form-check-input' type="checkbox" name="enabled" value="yes" id="checkboxEnabled" {{ $plugin->isEnabled() ? 'checked' : '' }}>
+                    <label class='form-check-label' for="checkboxEnabled" >You will not be able to use the plugin, but it will keep and display the saved information.</label>
+                </div>
+            </div>
+            <br>
+            <button type="submit" class='btn btn-warning'>Update plugin</button>
+            <a href="{{ route('plugins.index') }}" class='btn btn-primary'>Back</a>
+        </form>
     </div>
-    <br>
-    <button type="submit">Update plugin</button>
-    <a href="{{ route('plugins.index') }}">Back</a>
-</form>
-<hr>
-<form action="{{ route('plugins.destroy', $plugin) }}" method="post">
-    @csrf
-    @method('delete')
-    <button type="submit">Delete plugin</button>
-</form>
+</div>
+<br>
+<x-modal id='modalDeletePlugin' title='Delete plugin'>
+    <x-slot name='trigger' class='link-danger' align='end'>Delete plugin</x-slot>
+    <div class="text-center">
+        <p class="lead">{{ $plugin->name }}</p>
+        <p>How many works has it?...</p>
+        <p class="text-muted">Are you sure you want to remove the plugin?</p>
+    </div>
+    <x-slot name='footer' close='Cancel'>
+        <form action="{{ route('plugins.destroy', $plugin) }}" method="post">
+            @csrf
+            @method('delete')
+            <button type="submit" class='btn btn-outline-danger'>Delete plugin</button>
+        </form>
+    </x-slot>
+</x-modal>
 @endsection
--- resources/views/plugins/index.blade.php
+++ resources/views/plugins/index.blade.php
@@ -1,38 +1,44 @@
 @extends('app')
 @section('content')
-<h1>Plugins ({{ $api_plugins->count() }})</h1>
-<table>
-    <thead>
-        <tr>
-            <th>Name</th>
-            <th>Catalog</th>
-            <th>Price</th>
-            <th></th>
-        </tr>
-    </thead>
-    <tbody>
-        @foreach($api_plugins as $api_plugin)
-        <tr>
-            <td>
-                <span>{{ $api_plugin->name }} v{{ $api_plugin->version }}</span>
-                <br>
-                <small>{{ $api_plugin->description }}</small>
-            </td>
-            <td>{{ ucfirst($api_plugin->catalog->name) }}</td>
-            <td>{{ $api_plugin->isFree() ? 'Free' : $api_plugin->price }}</td>
-            <td>
-                @if( $plugins->contains('api_plugin_id', $api_plugin->id) )
-                <a href="{{ route('plugins.edit', $plugins->firstWhere('api_plugin_id', $api_plugin->id) ) }}">Edit</a>
-
-                @else
-                <button type="submit" name="plugin" value="{{ $api_plugin->hashed }}" form="formPluginStore">Purchase</button>
-
-                @endif
-            </td>
-        </tr>
-        @endforeach
-    </tbody>
-</table>
+<x-heading>Plugins ({{ $api_plugins->count() }})</x-heading>
+<div class="card">
+    <div class="card-body">
+        <div class="table-responsive">
+            <table class='table table-hover align-middle shadow-none'>
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Catalog</th>
+                        <th>Price</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($api_plugins as $api_plugin)
+                    <tr>
+                        <td>
+                            <span>{{ $api_plugin->name }} v{{ $api_plugin->version }}</span>
+                            <br>
+                            <small>{{ $api_plugin->description }}</small>
+                        </td>
+                        <td>{{ ucfirst($api_plugin->catalog->name) }}</td>
+                        <td>{{ $api_plugin->isFree() ? 'Free' : "$ {$api_plugin->price}" }}</td>
+                        <td class='text-end'>
+                            @if( $plugins->contains('api_plugin_id', $api_plugin->id) )
+                            <a href="{{ route('plugins.edit', $plugins->firstWhere('api_plugin_id', $api_plugin->id) ) }}" class='btn btn-warning'>Edit</a>
+            
+                            @else
+                            <button type="submit" name="plugin" value="{{ $api_plugin->hashed }}" form="formPluginStore" class='btn btn-success'>Purchase</button>
+            
+                            @endif
+                        </td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
 
 @if( $api_plugins->count() )
 <form action="{{ route('plugins.store') }}" method="post" id="formPluginStore">
