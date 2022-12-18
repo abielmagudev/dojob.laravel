a5419370f9e39207a28114f009fa8c87960db1d8
Abiel Magu <abielmagu@mail.com>
2022-12-18 04:12:40 -0600

    Factoriza carpeta de controllers de ApiPlugins

---
+++ app/Http/Controllers/ApiPlugins/PredectiveMaintenanceController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\ApiPlugins;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class PredectiveMaintenanceController extends Controller
+{
+    //
+}
