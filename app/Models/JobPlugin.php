fd7f2ddab7d7ee847c85bb0230cafcbb0e11187f
Abiel Magu <abielmagu@mail.com>
2023-01-19 02:12:45 -0600

    Agrega funcionalidad a model JobPlugin

--- app/Models/JobPlugin.php
+++ app/Models/JobPlugin.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Relations\Pivot;
 
 class JobPlugin extends Pivot
 {
-
+    public function api()
+    {
+        return $this->hasMany(ApiPlugin::class, 'id', 'plugin_id');
+    }
 }
